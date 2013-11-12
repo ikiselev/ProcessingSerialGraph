c7112fa581404c8ddc600f43f7227b25ff6985b8
ivan.kiselev <vanwin@yandex.ru>
2013-11-12 23:55:45 +0400

    Slowly scroll method mock data to graph

--- test/ProcessingSerialGraphTest.java
+++ test/ProcessingSerialGraphTest.java
@@ -12,22 +12,23 @@ public class ProcessingSerialGraphTest {
         PApplet.runSketch(args, window);
 
         window.graph.initColumnNames("TestColumns:gyro.x{-32768;32767},gyro.y{-32768;32767},gyro.z{-32768;32767},acc.x{-2048;2047},acc.y{-2048;2047},acc.z{-2048;2047}", 1200);
-        window.graph.processData("-72,-59,-78,-69,11,-1011|100");
-        window.graph.processData("-17,-32,-71,-61,-5,-1026|200");
-        window.graph.processData("-32,-71,-78,-55,0,-1021|300");
-        window.graph.processData("-65,-40,-35,-54,1,-1000|400");
-        window.graph.processData("-2,-79,-61,-46,-13,-1014|500");
-        window.graph.processData("-11,-63,-80,-45,-10,-1014|600");
-        window.graph.processData("827,-13855,342,35,268,-1109|700");
-        window.graph.processData("3063,-17567,367,-89,191,-1171|800");
-        window.graph.processData("6892,-18698,4,-117,191,-1095|900");
-        window.graph.processData("8245,-16985,96,-117,184,-1095|1100");
-        window.graph.processData("9143,-13561,-145,-231,191,-1146|1200");
-        window.graph.processData("8770,3649,254,-214,194,-1030|1300");
-        window.graph.processData("7630,8853,502,-214,194,-1030|1400");
-        window.graph.processData("3834,7811,710,-130,109,-829|1500");
-        window.graph.processData("1784,12073,757,-229,97,-976|1600");
-        window.graph.processData("-2296,25494,594,-152,65,-909|1700");
+        processData(window, "-72,-59,-78,-69,11,-1011|100");
+        processData(window, "-17,-32,-71,-61,-5,-1026|200");
+        processData(window, "-32,-71,-78,-55,0,-1021|300");
+        processData(window, "-65,-40,-35,-54,1,-1000|400");
+        processData(window, "-2,-79,-61,-46,-13,-1014|500");
+        processData(window, "-11,-63,-80,-45,-10,-1014|600");
+        processData(window, "827,-13855,342,35,268,-1109|700");
+        processData(window, "3063,-17567,367,-89,191,-1171|800");
+        processData(window, "6892,-18698,4,-117,191,-1095|900");
+
+        processData(window, "8245,-16985,96,-117,184,-1095|1100");
+        processData(window, "9143,-13561,-145,-231,191,-1146|1200");
+        processData(window, "8770,3649,254,-214,194,-1030|1300");
+        processData(window, "7630,8853,502,-214,194,-1030|1400");
+        processData(window, "3834,7811,710,-130,109,-829|1500");
+        processData(window, "1784,12073,757,-229,97,-976|1600");
+        processData(window, "-2296,25494,594,-152,65,-909|1700");
 
 
         /**
@@ -37,5 +38,14 @@ public class ProcessingSerialGraphTest {
         {
 
         }
+    }
+
+    protected void processData(ProcessingSerialGraph window, String sData)
+    {
+        /**
+         * Visualize graph
+         */
+        window.graph.processData(sData);
+        window.delay(500);
     }
 }
