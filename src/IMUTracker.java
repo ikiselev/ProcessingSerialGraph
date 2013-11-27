5864cb54216f58890af49bfdc0aef02871bbfed2
Ivan Kiselev <ivan.kiselev@wikimart.ru>
2013-11-27 20:11:14 +0400

    IMUTracker

---
+++ src/IMUTracker.java
@@ -0,0 +1,93 @@
+import Preprocessors.P3DPreprocessor;
+import processing.core.PApplet;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+//TODO: Remove copy-pase from GyroDrift
+public class IMUTracker extends ProcessingApplet
+{
+    int dataProcessorWidth = 2;
+
+    int textOffest = 0;
+
+    float gyroAngleX;
+    float gyroAngleY;
+    float gyroAngleZ;
+
+    static public void main(String args[]) {
+        PApplet.main(new String[]{"IMUTracker"});
+    }
+
+    public void setup()
+    {
+        super.setup();
+        dataProcessor.addPreprocessor(new P3DPreprocessor());
+    }
+
+    public void draw()
+    {
+        background(0xff000000);
+        textSize(25);
+        lights();
+
+
+        textOffest = 0;
+        drawBarText("X axis", gyroAngleX);
+        drawBarText("Y axis", gyroAngleY);
+        drawBarText("Z axis", gyroAngleZ);
+
+        pushMatrix();
+
+        translate(windowWidth / 2, windowHeight / 2, 0);
+
+        rotateX(radians(gyroAngleX));
+        rotateY(radians(gyroAngleZ)); //it is normal Z axis on Y
+        rotateZ(radians(gyroAngleY));
+
+        drawBox();
+        popMatrix();
+
+    }
+
+    public void drawBox()
+    {
+        box(100, 12, 400);
+    }
+
+    public void drawBarText(String text, float angle)
+    {
+        DecimalFormat df = new DecimalFormat("#.##");
+        df.setRoundingMode(RoundingMode.HALF_UP);
+        df.setMinimumFractionDigits(2);
+        text(text + ": " + df.format(angle), 100 + textOffest, 20);
+        textOffest += 200;
+    }
+
+    public void mouseClicked() {
+        gyroAngleX = 0.0f;
+        gyroAngleY = 0.0f;
+        gyroAngleZ = 0.0f;
+    }
+
+    public void processData (String serialData)
+    {
+        serialData = trim(serialData);
+        if (serialData != null && !serialData.equals("")) {
+            dataProcessor.processData(serialData);
+        }
+
+        gyroAngleX -= dataProcessor.graphData.COLUMN_DATA[0][getDataProcessorWidth() - 1];
+        gyroAngleY -= dataProcessor.graphData.COLUMN_DATA[1][getDataProcessorWidth() - 1];
+        gyroAngleZ += dataProcessor.graphData.COLUMN_DATA[2][getDataProcessorWidth() - 1];
+    }
+
+    public String getRenderer()
+    {
+        return P3D;
+    }
+
+    public int getDataProcessorWidth() {
+        return dataProcessorWidth;
+    }
+}
